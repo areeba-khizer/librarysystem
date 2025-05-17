83e8b4e44fd5b2d5eda4440e329eaa50779e2105
areeba khizer <areebakhizer7@outlook.com>
2025-05-17 22:49:01 +0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,21 @@
 set -e
 # === Configuration ===
 REMOTE_HOST="ubuntu@54.172.53.178"
+
 COMPOSE_DIR="/home/ubuntu/librarysystem"
 
+if [ -z "$1" ]; then
+  echo "Error: IMAGE_TAG not provided."
+  echo "Usage: ./deploy.sh <image-tag>"
+  exit 1
+fi
+
+IMAGE_TAG="$1"
 # === SSH Command ===
 ssh -o StrictHostKeyChecking=no "$REMOTE_HOST" << EOF
   cd "$COMPOSE_DIR"
+  export IMAGE_TAG="$IMAGE_TAG"
+  echo "$IMAGE_TAG"
   echo "Bringing down service"
   sudo docker-compose -p library-ci -f docker-compose-ci.yml down
   echo "Updating image"
