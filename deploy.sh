aa355ae5a0f85141308927088b4f2e289ccc14a4
areeba khizer <areebakhizer7@outlook.com>
2025-05-17 22:29:30 +0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
- 
 set -e
- 
 # === Configuration ===
 REMOTE_HOST="ubuntu@54.172.53.178"
 COMPOSE_DIR="/home/ubuntu/librarysystem"
- 
+
 # === SSH Command ===
-ssh -i -o StrictHostKeyChecking=no "$REMOTE_HOST" << EOF
+ssh -o StrictHostKeyChecking=no "$REMOTE_HOST" << EOF
   cd "$COMPOSE_DIR"
   echo "Bringing down service"
   sudo docker-compose -p library-ci -f docker-compose-ci.yml down
   echo "Updating image"
   sudo docker-compose -p library-ci -f docker-compose-ci.yml pull
   echo "Starting service"
-  sudo docker-compose -p library-ci -f docker-compose-ci.yml up
+  sudo docker-compose -p library-ci -f docker-compose-ci.yml up -d
+  echo "Deployment complete."
+EOF
